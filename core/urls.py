afd692885807dcc6898963bffe5391c9f76479c9
Atapkel Beibarys <beibarysatapkel@gmail.com>
2025-05-11 20:59:50 +0500

    some changes

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import predict_role_view
 
 urlpatterns = [
-    path('predict-role/', predict_role_view, name='predict_role'),
+    path('', predict_role_view, name='predict_role'),
 ]
